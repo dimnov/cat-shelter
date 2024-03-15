131ac618056d8e787e644e58e76e9e7e3fe86bc4
dimnov <dimnov1512@gmail.com>
2024-03-15 18:48:36 +0200

    Add paths

--- templates/catTemplate.js
+++ templates/catTemplate.js
@@ -6,8 +6,8 @@ const catTemp = (cat) => `
 <p><span>Breed: </span>${cat.breed}</p>
 <p><span>Description: </span>${cat.description}</p>
 <ul class="buttons">
-  <li class="btn edit"><a href="">Change Info</a></li>
-  <li class="btn delete"><a href="">New Home</a></li>
+  <li class="btn edit"><a href="/cats/edit-cat/${cat.id}">Change Info</a></li>
+  <li class="btn delete"><a href="/cats/delete-cat/${cat.id}">New Home</a></li>
 </ul>
 </li>
 `;
