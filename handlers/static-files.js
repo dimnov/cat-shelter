15e09cf7bb830632f846501b6d2c3aae05da4e3c
dimnov <dimnov1512@gmail.com>
2024-03-15 18:47:10 +0200

    Add handler for static files

--- handlers/static-files.js
+++ handlers/static-files.js
@@ -1,24 +1,45 @@
 const fs = require('fs/promises');
+const path = require('path');
 
-async function handleCss(req, res) {
-  const homeViewCss = await fs.readFile('./content/styles/site.css');
+async function handleStaticFile(req, res) {
+  // const homeViewCss = await fs.readFile('./content/styles/site.css');
+  let filePath = '';
 
-  res.writeHead(200, {
-    'Content-Type': 'text/css'
-  });
-  res.write(homeViewCss);
-  res.end();
+  if (req.url === '/content/styles/site.css') {
+    filePath = './content/styles/site.css';
+  } else if (req.url === '/content/images/pawprint.ico') {
+    filePath = './content/images/pawprint.ico';
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+    return;
+  }
+
+  try {
+    const fileData = await fs.readFile(filePath);
+    const contentType = getContentType(filePath);
+
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(fileData);
+  } catch (error) {
+    console.error('Error serving static file:', error);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  }
 }
 
-async function handleIcon(req, res) {
-  const image = await fs.readFile('./content/images/pawprint.ico');
-
-  res.writeHead(200, {
-    'Content-Type': 'image'
-  });
-  res.write(image);
-  res.end();
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.css':
+      return 'text/css';
+    case '.ico':
+      return 'image/x-icon';
+    default:
+      return 'text/plain';
+  }
 }
 
 
-module.exports = { handleCss, handleIcon };
+
+module.exports = { handleStaticFile };
